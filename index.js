bba1cfb304dd8e63818578627762323949facb7e
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-05 00:59:49 +0600

    menu data has successfully added

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+const cors = require("cors");
+
+//middle war
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.dracezw.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    //menu data api built here
+    const menuCollection = client.db("Restaurant").collection("menu");
+    // console.log("collection", menuCollection);
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("I am running on ...5000 server for Restaraunts ");
+});
+app.listen(port, () => {
+  console.log(`I am running on here ${port}`);
+});
